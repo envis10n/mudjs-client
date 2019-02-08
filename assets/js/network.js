f4ed3c76aa031a3fea598a1ac3c1ddcfa5477c34
envis10n <envis10n@protonmail.com>
2019-02-08 10:07:47 -0600

    encode print payload

--- assets/js/network.js
+++ assets/js/network.js
@@ -22,7 +22,7 @@ $(function(){
                 break;
                 case "print":
                     if (terminal) {
-                        terminal.write(dobj.payload);
+                        terminal.write(html.encode(dobj.payload));
                     }
                 break;
                 case 'prompt':
